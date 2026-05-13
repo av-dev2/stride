8fd099ae8650fdd6d9031562e9b404f97b6f0fe0
av-dev2 <emgani@aakvatech.com>
2026-05-14 02:16:36 +0300

    feat: add Stride Settings single doctype

---
+++ stride/stride/doctype/stride_settings/stride_settings.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2024, elius-dev and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class StrideSettings(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		enable_auto_invoicing: DF.Check
+		gps_api_key: DF.Password
+		gps_api_url: DF.Data | None
+		gps_polling_interval_minutes: DF.Int
+		rental_item: DF.Link | None
+	# end: auto-generated types
+
+	pass
