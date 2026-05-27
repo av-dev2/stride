33db2587584a140414abc3c4b39292e7db7e9d58
av-dev2 <emgani@aakvatech.com>
2026-05-27 21:45:14 +0300

    feat(stride): add Payment Log DocType for tracking payments
    Add new Payment Log DocType with fields for posting_date, posting_time,
    paid_amount, payment_method, paid_to, description, and a reconciled
    checkbox. Uses random hash naming and includes search indexes on key
    fields for efficient filtering. Permissions granted to System Manager.

---
+++ stride/stride/doctype/payment_log/payment_log.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, elius-dev and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PaymentLog(Document):
+	pass
