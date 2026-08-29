725a0f8254b6f6102e094c9442d3c6bd60cc9049
av-dev2 <emgani@aakvatech.com>
2026-08-30 00:33:30 +0300

    test: migrate Payment Log test off deprecated FrappeTestCase

--- stride/stride/doctype/payment_log/test_payment_log.py
+++ stride/stride/doctype/payment_log/test_payment_log.py
@@ -1,9 +1,27 @@
 # Copyright (c) 2026, elius-dev and Contributors
 # See license.txt
 
-# import frappe
-from frappe.tests.utils import FrappeTestCase
+import frappe
+from frappe.tests import IntegrationTestCase
+from frappe.utils import now_datetime
 
 
-class TestPaymentLog(FrappeTestCase):
-	pass
+class TestPaymentLog(IntegrationTestCase):
+	def test_can_insert_with_its_declared_fields(self):
+		doc = frappe.get_doc(
+			{
+				"doctype": "Payment Log",
+				"posting_date": now_datetime().date(),
+				"posting_time": now_datetime().time(),
+				"paid_amount": 150.0,
+				"payment_method": "Cash",
+				"paid_to": "Test Cash Account",
+				"description": "Test payment log entry",
+			}
+		)
+		doc.insert(ignore_permissions=True)
+		self.addCleanup(lambda: frappe.delete_doc("Payment Log", doc.name, ignore_permissions=True))
+
+		doc.reload()
+		self.assertEqual(doc.paid_amount, 150.0)
+		self.assertFalse(doc.reconciled)
